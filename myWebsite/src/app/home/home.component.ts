7f66e285d191675f5d0ce70968cdad52a8340215
Prince2802 <chaudhary.prince97@gmail.com>
2024-06-16 23:21:01 +0530

    home page

---
+++ myWebsite/src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {HomeService} from "./home.service";
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html'
+})
+export class HomeComponent implements OnInit{
+  public age: number = 0;
+
+  constructor(private homeService: HomeService) {}
+
+  ngOnInit() {
+    this.age = this.homeService.getAge();
+  }
+}
