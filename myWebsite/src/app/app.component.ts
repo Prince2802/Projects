db0446034ad2466711f17e83a9a8ee92e95b2da3
Prince2802 <chaudhary.prince97@gmail.com>
2024-06-17 03:11:47 +0530

    created navbar

--- myWebsite/src/app/app.component.ts
+++ myWebsite/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { GoogleAnalyticsService } from './services/google-analytics.service';
+import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'My Personal Website';
+export class AppComponent implements AfterViewInit {
+  constructor(
+    private themeService: ThemeService,
+    public googleAnalyticsService: GoogleAnalyticsService
+  ) {}
+
+  ngAfterViewInit(): void {
+    this.themeService.setThemeSettings();
+  }
 }
